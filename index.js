c42f0ea581564b658d2e3acc9a593a46b4dcbbc9
Wayne <enyaw0215@gmail.com>
2020-05-12 03:43:11 +0000

    added quiz that displays questions and gives feed back, also displays score

--- index.js
+++ index.js
@@ -44,28 +44,128 @@ function getQuestion(qId){
     return new Promise(function(resolve,reject){
        connection.query(stmt,[qId],function(error, results) {
            if(error) throw error;
-           resolve(results);
+           resolve(results[0]);
        });
     });
 }
+
+//creates the quiz in a random order
+async function buildQuiz()
+{
+    let qs = [];
+    qs.push(await getQuestion(1));
+    qs.push(await getQuestion(2));
+    qs.push(await getQuestion(3));
+    return new Promise(function(resolve,reject){
+    //random integer from 1 to 3
+    let qId = (Math.floor(Math.random()*10) % 3);
+    //random number 1 or 0 1 means the next question will be one less 0 means it will be 1 more
+    let next = (Math.floor(Math.random()*10) % 2);
+    let quiz = [];
+    quiz.push(qs[qId]);
+    if(next == 0)
+    {
+        qId++;
+        if(qId>2)
+        {
+            qId = 0;
+        }
+        quiz.push(qs[qId]);
+        qId++;
+        if(qId>2)
+        {
+            qId = 0;
+        }
+        quiz.push(qs[qId]);
+    }
+    else
+    {
+        qId--;
+        if(qId<0)
+        {
+            qId = 2;
+        }
+        quiz.push(qs[qId]);
+        qId--;
+        if(qId<0)
+        {
+            qId = 2;
+        }
+        quiz.push(qs[qId]);
+    }
+        resolve(quiz);
+    });
+}
+
+function isAuthenticated(req, res, next)
+{
+    if(!req.session.authenticated) res.redirect('/home');
+    else next();
+}
+
 //looks for url matching the string and does the function
 // looks for a /
 app.get("/", function(req,res){
     res.render("home.ejs");
 });
-
-
+//isAuthenticated
 app.get("/quiz", async function(req, res) {
-    let quiz = await getQuestion(1);
-    res.render("quiz.ejs", {quiz:quiz[0]});
+    console.log(req.session.q > 2);
+    if(req.session.q > 2)
+    {
+        res.redirect("/complete");
+    }
+    else
+    {
+        res.render("quiz.ejs", {quiz:req.session.quiz[req.session.q],qNumber:req.session.q,score:req.session.score});
+    }
 });
 
 app.post("/quiz", async function(req,res){
-    console.log(req.body);    
-    console.log(req.body.choice);    
-    res.redirect("/quiz");
+    let isCorrect = false;
+    if(req.body.choice == req.session.quiz[req.session.q].answer)
+    {
+        req.session.score++;
+        isCorrect = true;
+    }
+    req.session.q++;
+    //no more questions
+    res.render("answer.ejs",{quiz:req.session.quiz[req.session.q-1],qNumber:req.session.q,score:req.session.score,choice:req.body.choice, isCorrect:isCorrect,});
 });
 
+//login routes
+app.get("/login", function(req, res){
+   res.render("login.ejs"); 
+});
+
+app.post("/login", async function(req, res){
+    req.session.authenticated = true;
+    req.session.email = req.body.email;
+    req.session.quiz = (await buildQuiz());
+    req.session.q = 0;
+    req.session.score = 0;
+    
+    res.redirect("/quiz");
+    /*
+    let users = await checkUsername(req.body.username);
+    let hashedPassword = users.length > 0 ? users[0].password : '';
+    let passwordMatch = await checkPassword(req.body.password, hashedPassword);
+    if(passwordMatch){
+        req.session.authenticated = true;
+        req.session.user = users[0].username;
+        req.session.isAdmin = users[0].isAdmin;
+        res.redirect('/welcome');
+    }
+    else{
+        res.render('login.ejs', {error: true});
+    }
+    */
+});
+
+app.get("/complete",function(req,res)
+{
+    res.render("complete.ejs",{score:req.session.score,qNumber:req.session.q});
+});
 
 // regex star takes everything this should be last so that other options are hit first
 app.get("*", function(req, res){
