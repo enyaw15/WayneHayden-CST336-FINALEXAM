4bfd886607e4162e075e2415ccccbd6087f20e2e
Wayne Hayden <enyaw0215@gmail.com>
2020-05-11 22:08:37 -0700

    remove un-need comments

--- index.js
+++ index.js
@@ -14,14 +14,6 @@ app.use(session({
 app.set('view engine', 'ejs');//set the view engine so we can use ejs
 
 //create sql server 
-/*
-const connection = mysql.createConnection({
-    host:"localhost",
-    user: "enyaw",
-    password: "0215Enyaw!",
-    database: "quiz_db"
-});
-*/
 //mysql://beb956d049b1b0:4ce83054@us-cdbr-east-06.cleardb.net/heroku_88219e38d1dcf7d?reconnect=true
 //reset the database
 //mysql -u b081679114026f --password=23b7c662 -h us-cdbr-east-06.cleardb.net heroku_ba1f10e7295fc08 < sql/quiz.sql
